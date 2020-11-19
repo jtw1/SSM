8a2978d99675010220ed640066445e34c1cc4509
jtw1 <15733168361@163.com>
2020-11-19 23:32:15 +0800

    2020.11.19加

---
+++ src/main/java/MVC/dao/BookDao.java
@@ -0,0 +1,14 @@
+package MVC.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Description
+ * @date 2020/11/19-22:57
+ */
+@Repository
+public class BookDao {
+    public void saveBook(){
+        System.out.println("图书已保存。。。");
+    }
+}
