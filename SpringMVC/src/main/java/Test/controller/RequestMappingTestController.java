9ca5ec303d75b2456f3308ab8607d8f21bc54a9b
jtwPC <15733168361@163.com>
2020-11-29 20:32:20 +0800

    ssm 2020.11.29

---
+++ SpringMVC/src/main/java/Test/controller/RequestMappingTestController.java
@@ -0,0 +1,32 @@
+package Test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @Description
+ * @date 2020/11/29-16:58
+ */
+@RequestMapping("/haha")  //为当前所有方法的请求地址指定一个基准路径
+@Controller
+public class RequestMappingTestController {
+    @RequestMapping("/handle01")
+    public String handle01(){
+        System.out.println("RequestMappingTestController...");
+        return "success";
+    }
+
+    //RequestMapping其他属性
+    @RequestMapping(value = "/handle02",method = {RequestMethod.POST})
+    public String handle02(){
+        System.out.println("RequestMappingTestController02...");
+        return "success";
+    }
+    //请求必须包含username，没带都会404
+    @RequestMapping(value = "/handle03",params = {"username"})
+    public String handle03(){
+        System.out.println("RequestMappingTestController-03...");
+        return "success";
+    }
+}
--- SpringMVC/src/main/webapp/index.jsp
+++ SpringMVC/src/main/webapp/index.jsp
@@ -1,6 +1,16 @@
+<%@ page language="java" pageEncoding="utf-8"%>
 <html>
 <body>
 <a href="hello">helloWorld</a>
 <h2>Hello World!</h2>
+<h1>RequestMappingTestController测试</h1>
+<a href="haha/handle01">test01-写在方法上的RequestMapping</a>
+<h1>RequestMappingTestController属性测试</h1>
+
+<a href="haha/handle02">handle02</a><br/>
+<form action="haha/handle02" method="post">
+    <input type="submit"/>
+</form>
+<a href="haha/handle03">handle03</a>
 </body>
 </html>
